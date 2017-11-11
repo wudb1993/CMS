c3af35918b5a436e7ef2e30840c08fb928f482af
wudb1993 <wudb1993@163.com>
2017-11-11 09:05:29 +0800

    多数据源调试通过

--- src/main/webapp/page/news/newsAdd.js
+++ src/main/webapp/page/news/newsAdd.js
@@ -12,23 +12,51 @@ layui.config({
  	var editIndex = layedit.build('news_content');
  	var addNewsArray = [],addNews;
  	form.on("submit(addNews)",function(data){
- 		//是否添加过信息
-	 	if(window.sessionStorage.getItem("addNews")){
-	 		addNewsArray = JSON.parse(window.sessionStorage.getItem("addNews"));
-	 	}
-	 	//显示、审核状态
- 		var isShow = data.field.show=="on" ? "checked" : "",
- 			newsStatus = data.field.shenhe=="on" ? "审核通过" : "待审核";
+ 		var param ={
+ 			"title" : $(".newsName").val(),
+			"" : $(".newsLook option").eq($(".newsLook").val()).text(),
+            // var isShow = data.field.show=="on" ? "checked" : "",
+			"status" : $(".newsStatus").val()=="on" ? "0" : "1",
+            "general" : $(".newsStatus").val(),
+            "content" : $("#news_content").val()
+		}
+        $.ajax({
+            url:'/systemNotice/login.do',
+            type:'get',
+            cache:false,
+            async:false,
+            data:{param:JSON.stringify(param)},
+            dataType:"json",
+            success:function(data){
+                if(data.model.result=='success'){
+                    location.href = '/index.html';
+                }else{
 
- 		addNews = '{"newsName":"'+$(".newsName").val()+'",';  //文章名称
- 		addNews += '"newsId":"'+new Date().getTime()+'",';	 //文章id
- 		addNews += '"newsLook":"'+$(".newsLook option").eq($(".newsLook").val()).text()+'",'; //开放浏览
- 		addNews += '"newsTime":"'+$(".newsTime").val()+'",'; //发布时间
- 		addNews += '"newsAuthor":"'+$(".newsAuthor").val()+'",'; //文章作者
- 		addNews += '"isShow":"'+ isShow +'",';  //是否展示
- 		addNews += '"newsStatus":"'+ newsStatus +'"}'; //审核状态
- 		addNewsArray.unshift(JSON.parse(addNews));
- 		window.sessionStorage.setItem("addNews",JSON.stringify(addNewsArray));
+                }
+
+            },
+
+        })
+
+ 		//是否添加过信息
+	 	// if(window.sessionStorage.getItem("addNews")){
+	 	// 	addNewsArray = JSON.parse(window.sessionStorage.getItem("addNews"));
+	 	// }
+	 	//显示、审核状态
+ 		// var isShow = data.field.show=="on" ? "checked" : "",
+ 		// 	newsStatus = data.field.shenhe=="on" ? "审核通过" : "待审核";
+ 		//
+         //
+ 		// addNews = '{"newsName":"'+$(".newsName").val()+'",';  //文章名称
+ 		// addNews += '"newsId":"'+new Date().getTime()+'",';	 //文章id
+ 		// addNews += '"newsLook":"'+$(".newsLook option").eq($(".newsLook").val()).text()+'",'; //开放浏览
+ 		// addNews += '"newsTime":"'+$(".newsTime").val()+'",'; //发布时间
+ 		// addNews += '"newsAuthor":"'+$(".newsAuthor").val()+'",'; //文章作者
+ 		// addNews += '"isShow":"'+ isShow +'",';  //是否展示
+ 		// addNews += '"newsStatus":"'+ newsStatus +'"}'; //审核状态
+ 		// addNewsArray.unshift(JSON.parse(addNews));
+ 		// window.sessionStorage.setItem("addNews",JSON.stringify(addNewsArray));
+
  		//弹出loading
  		var index = top.layer.msg('数据提交中，请稍候',{icon: 16,time:false,shade:0.8});
         setTimeout(function(){
